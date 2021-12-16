eaa8b76ce26838e087c433d84ed4463b593d650f
TEJAAL M <tejaalrani@gmail.com>
2021-12-16 09:08:27 +0530

    patch the division function

--- main.go
+++ main.go
@@ -1,11 +1,21 @@
 package explorepackage
 
-func add(a,b int)int{
-	return a+b
+import "errors"
+
+func add(a, b int) int {
+	return a + b
 }
-func sub(a,b int)int{
-	return a-b
+func sub(a, b int) int {
+	return a - b
 }
-func mult(a,b int)int{
-	return a*b
+func mult(a, b int) int {
+	return a * b
+}
+func div(a, b int) (int, error) {
+	if b == 0 {
+		return 0, errors.New("division by zero not valid")
+
+	}
+	return a / b, nil
+
 }
