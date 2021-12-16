d279063bb0576bc3bf74e242c0e8ae13b59f52c9
TEJAAL M <tejaalrani@gmail.com>
2021-12-16 08:34:25 +0530

    initial version

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/ENG19CS0334-TEJAAL-M/explorepackage.git
+
+go 1.17
---
+++ main.go
@@ -0,0 +1,11 @@
+package explorepackage
+
+func add(a,b int)int{
+	return a+b
+}
+func sub(a,b int)int{
+	return a-b
+}
+func mult(a,b int)int{
+	return a*b
+}
